73795eaa41badc49247d34dc054a292bea29a806
Shlok-Goswami <145427358+Shlok-Goswami@users.noreply.github.com>
2025-01-10 13:49:02 +0530

    adding gitignore file and proper working with node

--- js.js
+++ js.js
@@ -2,7 +2,26 @@ const spawner = require('child_process').spawn;
 
 const python_process = spawner('python', ['./main.py', JSON.stringify([0])]);
 
+function copy(arr){
+    let ans=[];
+    let i=0;
+     arr.forEach(e => {
+        ans.push(e);
+    });
+    return ans;
+}
+let arr=[];
 python_process.stdout.on('data',(data) =>{
 
-    console.log("python output in js: "+data);
+    data=(data+"").split(',');
+    arr=copy(data);
 });
+let myInterval = setInterval(myFunc,100)
+
+
+
+function myFunc(){
+    if(arr[0]!=undefined){
+        console.log("python output in js: index = ("+arr[0]+","+arr[1]+")  middle = ("+arr[2]+","+arr[3]+")");
+    }
+}
--- main.py
+++ main.py
@@ -11,10 +11,9 @@ mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
 
 cap = cv2.VideoCapture(0)  # Use the webcam
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(3, 600)  # Set width (double the value for portrait orientation)
-cap.set(4, 900)  # Set height (double the value for portrait orientation)
+
+cap.set(3, 1000)  # Set width (double the value for portrait orientation)
+cap.set(4, 2000)  # Set height (double the value for portrait orientation)
 
 frame_count = 0
 while cap.isOpened():
@@ -39,14 +38,15 @@ while cap.isOpened():
                 index_finger_tip = hand_landmarks.landmark[8]
                 h, w, _ = frame.shape
                 index_x, index_y = int(index_finger_tip.x * w), int(index_finger_tip.y * h)
-                print(f"Index Finger Tip: (x={index_x}, y={index_y})")
+                #print(f"Index Finger Tip: (x={index_x}, y={index_y})")
                 finger_coordinates.append((index_x, index_y))  # Store index finger coordinates
 
                 # Middle finger tip (Landmark 12)
                 middle_finger_tip = hand_landmarks.landmark[12]
                 middle_x, middle_y = int(middle_finger_tip.x * w), int(middle_finger_tip.y * h)
-                #print(f"Middle Finger Tip: (x={middle_x}, y={middle_y})")
+                print(f"{index_x},{index_y},{middle_x},{middle_y}")
                 finger_coordinates.append((middle_x, middle_y))  # Store middle finger coordinates
+                #print(f""+index_x+","+index_y+","+middle_x+","+middle_y)
 
                 # Optionally draw the fingertips on the frame
                 cv2.circle(frame, (index_x, index_y), 5, (0, 255, 0), -1)  # Green for index finger
@@ -57,7 +57,7 @@ while cap.isOpened():
                 input = ast.literal_eval(sys.argv[1])
                 output = input
 
-                output.append(finger_coordinates)
+                output.append(finger_coordinates) 
                 sys.stdout.flush()
 
         # Display the frame
